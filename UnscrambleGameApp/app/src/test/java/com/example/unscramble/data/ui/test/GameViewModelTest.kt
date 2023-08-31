6eafa22b7ff3235f640734198d9b53e67db46033
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2023-08-31 23:09:55 +0700

    [Unit Test] boundary case

--- UnscrambleGameApp/app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ UnscrambleGameApp/app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -39,6 +40,10 @@ class GameViewModelTest {
 //    thingUnderTest = gameViewModel
 //    TriggerOfTest = CorrectWordGuessed
 //    ResultOfTest = ScoreUpdatedAndErrorFlagUnset
+
+
+    //[Error Path]
+    // check : score unchange and isGuessWordWrong = true
     @Test
     fun gameViewModel_IncorrectWordGuess_ErrorFlagSet(){
         val incorrectPlayerWord = "and"
@@ -51,9 +56,32 @@ class GameViewModelTest {
         assertEquals(0, currentGameUiState.score)
         // Assert that checkUserGuess() method updates isGuessedWordWrong correctly
         assertTrue(currentGameUiState.isGuessWordWrong)
+    }
 
+
+    //[Boundary case]
+    //when GameViewModel initialized :
+    //currentWordCount = 1, score = 0, isGuessWord = false, isGameOver = false
+
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded(){
+        val gameUiState = viewModel.uiState.value
+
+        val unScrambleWord = getUnscrambledWord(gameUiState.currentScrambleWord)
+        // Assert that current word is scrambled.
+        assertNotEquals(unScrambleWord, gameUiState.currentScrambleWord)
+        // Assert that currentWordCount is set to 1
+        assertEquals(1, gameUiState.currentWordCount)
+        // Assert that score = 0
+        assertEquals(0, gameUiState.score)
+        // Assert that isGuessWord = false
+        assertFalse(gameUiState.isGuessWordWrong)
+        //Assert that isGameOver = false
+        assertFalse(gameUiState.isGameOver)
 
     }
+
+
 
 
 }
